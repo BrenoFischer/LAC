523c4db4133591f16c4e454c82761a4d001fc7ab
BrenoFischer <breno_perricone@hotmail.com>
2021-02-24 16:57:10 -0300

    markerController e marcadores no GoogleMaps

--- lib/components/my_map.dart
+++ lib/components/my_map.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
+import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:flutter/foundation.dart';
+import 'package:lac/controllers/marker_controller.dart';
 
 class MyMap extends StatefulWidget {
   @override
@@ -12,23 +14,27 @@ class MyMap extends StatefulWidget {
 
 class _MyMapState extends State<MyMap> {
   Completer<GoogleMapController> _controller = Completer();
+  final MarkerController markerController = MarkerController.to;
 
   static final CameraPosition initialPosition = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(53.2839936, -9.1187864),
     zoom: 14.4746,
   );
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      mapType: MapType.hybrid,
-      initialCameraPosition: initialPosition,
-      onMapCreated: (GoogleMapController controller) {
-        _controller.complete(controller);
-      },
-      gestureRecognizers: Set()
-        ..add(
-          Factory<EagerGestureRecognizer>(() => EagerGestureRecognizer()),
-        ),
-    );
+    return GetX<MarkerController>(builder: (markerController) {
+      return GoogleMap(
+        markers: Set<Marker>.of(markerController.markers.values),
+        mapType: MapType.hybrid,
+        initialCameraPosition: initialPosition,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        gestureRecognizers: Set()
+          ..add(
+            Factory<EagerGestureRecognizer>(() => EagerGestureRecognizer()),
+          ),
+      );
+    });
   }
 }
--- lib/controllers/location_controller.dart
+++ lib/controllers/location_controller.dart
@@ -1,8 +1,10 @@
 import 'package:lac/classes/location.dart';
 import 'package:get/get.dart';
+import 'package:lac/controllers/marker_controller.dart';
 
 class LocationController extends GetxController {
   final locations = List<Location>().obs;
+  final MarkerController markerController = MarkerController.to;
 
   static LocationController get to => Get.find<LocationController>();
 
@@ -12,8 +14,15 @@ class LocationController extends GetxController {
   }
 
   addLocation(Location location) {
+    int id;
     clearLocations();
     locations.add(location);
+    if (location.data[0] < 0) {
+      id = 1;
+    } else {
+      id = 2;
+    }
+    markerController.addMarker(location.data[0], location.data[1], id);
   }
 
   clearLocations() {
